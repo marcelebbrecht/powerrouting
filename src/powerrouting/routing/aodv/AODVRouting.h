dae37bc4ea1e6bed7af87fb1837cffa252a167aa
Marcel Ebbrecht <marcel.ebbrecht@googlemail.com>
2017-07-20 19:31:14 +0200

    comments fixed

--- src/powerrouting/routing/aodv/AODVRouting.h
+++ src/powerrouting/routing/aodv/AODVRouting.h
@@ -1,20 +1,7 @@
-//
-// Copyright (C) 2014 OpenSim Ltd.
-// Author: Benjamin Seregi
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see http://www.gnu.org/licenses/.
-//
+// PowerRouting for OMNeT++ - AODV routing
+// Marcel Ebbrecht, marcel.ebbrecht@googlemail.com
+// free software, see LICENSE.md for details
+// derived from inetmanet-3.5, AODVRouting.h
 
 #ifndef __INET_AODVROUTING_H
 #define __INET_AODVROUTING_H
