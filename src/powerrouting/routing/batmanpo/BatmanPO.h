b01a648a8b47976b803490fceff4d0ebf924f962
Marcel Ebbrecht <marcel.ebbrecht@googlemail.com>
2017-07-22 11:32:34 +0200

    added batmanpo

---
+++ src/powerrouting/routing/batmanpo/BatmanPO.h
@@ -0,0 +1,51 @@
+// PowerRouting for OMNeT++ - BatmanPO routing
+// Marcel Ebbrecht, marcel.ebbrecht@googlemail.com
+// free software, see LICENSE.md for details
+
+#ifndef __BatmanPO_omnet_h__
+
+#define __BatmanPO_omnet_h__
+#include "../batman/BatmanMain.h"
+#include "inet/power/contract/IEnergyStorage.h"
+#include "inet/power/storage/SimpleEpEnergyStorage.h"
+#include "inet/power/contract/IEpEnergyStorage.h"
+
+typedef inet::power::IEpEnergyStorage IEpEnergyStorage;
+
+class BatmanPO : public Batman
+{
+  protected:
+    // parameters for energy-optimization
+    cModule *host = nullptr;
+    IEpEnergyStorage *energyStorage = nullptr;
+    double relativeCharge = 0;
+    double newWillingnessDouble = 0;
+    int newWillingness = 0;
+    double powerSensitivity = 0;
+    double powerSensitivityMin = 0;
+    double powerSensitivityMax = 0;
+    double powerSensitivityDefault = 0;
+    double powerBias = 0;
+    double powerBiasMin = 0;
+    double powerBiasDefault = 0;
+    double powerTrigger = 0;
+    double powerTriggerMin = 0;
+    double powerTriggerMax = 0;
+    double powerTriggerDefault = 0;
+    double powerTriggerDouble = 0;
+    int powerTriggerCalculated = 999999999;
+    int powerTriggerLast = 999999999;
+
+//  protected:
+//    // override initialization, RREP creation and forwarding
+//    void initialize(int stage) override;
+//    virtual bool process_hello(Batman_msg&, const nsaddr_t &, const nsaddr_t &, const int &) override;
+//
+//    // calculate the penalty and trigger and set willingness
+//    int calculateWillingness(int willingness);
+//    int calculateTrigger();
+//    void setWillingness(int willingnessValue);
+};
+
+#endif
+
